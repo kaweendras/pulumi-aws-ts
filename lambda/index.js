587db3e962decf937144ac2b268a5cbe3c111cde
kaweendras <salitha.wijerathna@gmail.com>
2025-03-04 16:34:21 +0530

    CHG | Improve error handling and logging in S3 upload function

--- lambda/index.js
+++ lambda/index.js
@@ -1,13 +1,23 @@
-
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
     console.log("Event: ", event);
     const bucketName = process.env.BUCKET_NAME;
-    const body = JSON.parse(event.body);
-    const key = `data-${Date.now()}.json`;
+    console.log("Bucket Name: ", bucketName);
 
+    let body;
+    try {
+        body = JSON.parse(event.body);
+    } catch (error) {
+        console.error("Error parsing event body: ", error);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "Invalid request body", error }),
+        };
+    }
+
+    const key = `data-${Date.now()}.json`;
     const params = {
         Bucket: bucketName,
         Key: key,
@@ -19,14 +29,16 @@ exports.handler = async (event) => {
         await s3.putObject(params).promise();
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: "Data added to S3 bucket" ,
+            body: JSON.stringify({
+                message: "Data added to S3 bucket",
                 data: {
                     bucketName,
                     key,
-                }
+                },
             }),
         };
     } catch (error) {
+        console.error("Error adding data to S3 bucket: ", error);
         return {
             statusCode: 500,
             body: JSON.stringify({ message: "Error adding data to S3 bucket", error }),
