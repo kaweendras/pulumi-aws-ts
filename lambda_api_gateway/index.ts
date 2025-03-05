0e240c492b96804d5071a935ef89272fa7ec1040
kaweendras <salitha.wijerathna@gmail.com>
2025-03-05 15:17:57 +0530

    CHG | Add S3 bucket creation to Lambda API Gateway setup

--- lambda_api_gateway/index.ts
+++ lambda_api_gateway/index.ts
@@ -2,8 +2,13 @@
 
 import * as aws from "@pulumi/aws";
 import * as apigateway from "@pulumi/aws-apigateway";
-
 import { infoHandlers } from "./handler";
+import { createS3Bucket } from "./resources/s3";
+// import { createDynamoTable } from "./resources/dynamo";
+
+//create resources
+//s3 bucket
+const bucket = createS3Bucket();
 
 // Create an API endpoint.
 const api = new apigateway.RestAPI("api", {
@@ -19,3 +24,4 @@ const api = new apigateway.RestAPI("api", {
 // Pulumi exports values
 // See these outputs using: pulumi stack output endpointUrl
 export const endpointUrl = api.url;
+export const bucketName = bucket.id;
