3d8474d8ff5541db2631aa64345cd19bea3508a3
kaweendras <salitha.wijerathna@gmail.com>
2025-03-05 15:56:38 +0530

    CHG | Add upload endpoint to API for base64 object uploads to S3

--- lambda_api_gateway/index.ts
+++ lambda_api_gateway/index.ts
@@ -2,21 +2,29 @@
 
 import * as aws from "@pulumi/aws";
 import * as apigateway from "@pulumi/aws-apigateway";
-import { infoHandlers } from "./handler";
+import { infoHandlers, s3Handlers } from "./handler";
 import { createS3Bucket } from "./resources/s3";
+import { uploadObjectBase64 } from "./helpers/s3.helper";
 // import { createDynamoTable } from "./resources/dynamo";
 
 //create resources
 //s3 bucket
 const bucket = createS3Bucket("pulumi-bucket");
 
-// Create an API endpoint.
+uploadObjectBase64(bucket, "data.txt", "YWE=");
+
+//Create an API endpoint.
 const api = new apigateway.RestAPI("api", {
   routes: [
     {
       path: "/",
       method: "GET",
       eventHandler: infoHandlers.HelloWorldHandler,
+    },
+    {
+      path: "/upload",
+      method: "POST",
+      eventHandler: s3Handlers.uploadToBuckeBase64,
     },
   ],
 });
