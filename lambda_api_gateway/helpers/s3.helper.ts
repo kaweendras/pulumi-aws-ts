2a8ff67d99ebb8021a5cf176e551d8ab7e006208
kaweendras <salitha.wijerathna@gmail.com>
2025-03-05 15:57:13 +0530

    CHG | Add base64 upload helper to S3 helper functions

--- lambda_api_gateway/helpers/s3.helper.ts
+++ lambda_api_gateway/helpers/s3.helper.ts
@@ -1,17 +1,33 @@
-import pulumi from "@pulumi/pulumi";
-import aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
 
 // Create an S3 Bucket object
 
 const uploadObject = (
-  bucketId: string,
+  bucket: aws.s3.BucketV2,
+
   objectName: string,
   filePath: string
 ) => {
   const bucketObject = new aws.s3.BucketObject(objectName, {
-    bucket: bucketId,
+    bucket: bucket.id,
     source: new pulumi.asset.FileAsset(filePath),
   });
+  return bucketObject;
 };
 
-export { uploadObject };
+//upload an object to the S3 bucket as a base64 encoded string
+const uploadObjectBase64 = (
+  bucket: aws.s3.BucketV2,
+  objectName: string,
+  base64String: string
+) => {
+  const bucketObject = new aws.s3.BucketObject(objectName, {
+    bucket: bucket.id,
+    source: new pulumi.asset.StringAsset(base64String),
+  });
+
+  return bucketObject;
+};
+
+export { uploadObject, uploadObjectBase64 };
